7432bfa7bdeb15169ba93ac844bb5f6a581ff685
Luneva Ekaterina <luneva0376@yandex.ru>
2023-10-17 08:22:44 +0700

    Add AutoTest GetCustomerBalance_IsNotNull

--- FinancialApi.Tests/AutoTest.cs
+++ FinancialApi.Tests/AutoTest.cs
@@ -1,42 +1,45 @@
 using Microsoft.AspNetCore.Mvc;
 using Moq;
+using System.Threading.Tasks;
 
 namespace FinancialApiTests
 {
 	public class Tests
 	{
 		Mock<ICustomerService> _service;
+		FinancialApiController _controller;
+		Random _rand;
 
 		[SetUp]
 		public void Setup()
 		{
 			_service = new Mock<ICustomerService>();
-		}
+			_controller = new FinancialApiController(_service.Object);
+			_rand = new Random();
 
-		[Test]
-		public void Test()
-		{
-			// Arrange
-			var controller = new FinancialApiController(_service.Object);
 			for (int i = 0; i < 50; i++)
 			{
 				_service.Setup(_service => _service.RegistrateCustomer("Luneva", "Ekaterina", "Evgenevna", "3/6/1998")).ReturnsAsync((Customer)null);
 			}
-			/*
+		}
+		
+		[Test]
+		public async Task GetCustomerBalance_IsNotNull()
+		{
+			// Arrange
+
 			// Act
-			List<Thread> threads = new List<Thread>();
+			var updating = _controller.UpdateCustomerBalance(_rand.Next(1, 49), _rand.Next(1, 1000));
+
 			for (int i = 0; i < 10; i++)
 			{
-				Thread thread = new Thread(controller.UpdateCustomerBalance(, 20));
-				threads.Add(thread);
-			}
-			foreach (var t in threads)
-			{
-				t.Start();
+				var task = Task.Run(() => updating);
+				await Task.Delay(2000);
 			}
 
 			// Assert
-			Assert.Equals(controller.GetCustomerBalance(1), 20);*/
+			var balance = await _controller.GetCustomerBalance(_rand.Next(1, 49));
+			Assert.IsNotNull(balance);
 		}
 	}
 }
